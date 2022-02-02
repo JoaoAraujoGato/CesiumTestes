d41a2c86d7e17ae4a875bf4a464a6480c68c179c
Joao <joaovitorara98@gmail.com>
2022-02-02 09:32:27 -0300

    Seletor de obra funcionando com nuvem de pontos

--- src/app.js
+++ src/app.js
@@ -1,59 +1,41 @@
-import React from "react";
-import { Cartesian3, Color, Ion, IonResource } from "cesium";
-import { Viewer, Entity, Cesium3DTileset } from "resium";
+import React, { useState } from "react";
+import { Cartesian3, createWorldTerrain, Ion, IonResource } from "cesium";
+import { Viewer, Entity, Cesium3DTileset, CameraFlyTo } from "resium";
 import "./App.css";
-
-// export default function App() {
-//   return (
-//     <Viewer full>
-//       <Entity
-//         name="Tokyo"
-//         position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
-//         point={{ pixelSize: 10, color: Color.WHITE }}
-//         description="hoge"
-//       />
-//     </Viewer>
-//   );
-// }
 
 Ion.defaultAccessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjYmFiNmE5Yi1hYmMxLTQ4MTYtOTFmYy0zOGVhNzg1MDgzODgiLCJpZCI6Nzc4MTcsImlhdCI6MTY0MTk5ODQ1NH0.aqwcpB-mIgcH5dRPxFYWsAOgNsZVW9mNxPMpFcf23Sk";
+  
+  export default function App() {
+    const terrainProvider = createWorldTerrain();
+    const positionBrasil = Cartesian3.fromDegrees(-45.9387, -17.9302, 20000000.0);
+    const positionBarragem8 = Cartesian3.fromDegrees(-46.907051, -19.681241, 5000.0);
+    const positionCapimBranco = Cartesian3.fromDegrees(-44.073327, -20.108577, 2000.0);
+    const [posicaoCamera, setPosicaoCamera] = useState(positionBrasil);
 
-export default function App() {
-  let viewer; // This will be raw Cesium's Viewer object.
-
-  const handleReady = (tileset) => {
-    if (viewer) {
-      viewer.zoomTo(tileset);
+    function handleClick(destination) {
+      setPosicaoCamera(destination);
     }
-  };
 
-  return (
-    <div className="Tudo">
-
+    return (
+    <div className="baseMap3D">
       <Viewer
         className="ViewerMap"
         full
-        ref={(e) => {
-          viewer = e && e.cesiumElement;
-        }}
+        terrainProvider={terrainProvider}
       >
-        <Cesium3DTileset
-          url={IonResource.fromAssetId(751563)}
-          onReady={handleReady}
-        />
+        <CameraFlyTo destination={posicaoCamera} /> 
+        <Cesium3DTileset url={IonResource.fromAssetId(751563)} />
       </Viewer>
-      <div className="Teste">
-        <input
-          placeholder="Nome"
-        />
-        <input
-          placeholder="Endereço"
-        />
-        <input
-          type="email"
-          placeholder="E-mail"
-              />
+      <div className="baseInputs">
+        <select>
+          <option selected onClick={()=> handleClick(positionBrasil)}>Selecione uma Obra</option>
+          <option onClick={()=> handleClick(positionBarragem8)}>Barragem 8</option>
+          <option onClick={()=> handleClick(positionCapimBranco)}>Nuvem de Pontos</option>
+        </select>
+        {/* <button onClick={()=> handleClick(positionBrasil)}>Brasil</button> */}
+        {/* <button onClick={()=> handleClick(positionBarragem8)}>Barragem 8</button> */}
+        {/* <button onClick={()=> handleClick(positionCapimBranco)}>Nuvem de Pontos</button> */}
       </div>
     </div>
   );
